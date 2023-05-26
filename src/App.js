c4faa45edda2721792597efc37c01305692f530e
falsofacu <facundotabarez0@gmail.com>
2023-05-26 18:06:46 -0300

    Started over withoud Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonText: 'color',
+      clicked: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({
+      clicked: this.state.clicked + 1
+    })
+    console.log(this.state)
+  }
+
+  render() {
+    return(
+      <header>
+        <button 
+        id="color-btn"
+        className="reset-button press-animation pulse-animation" 
+        onClick={this.handleClick}
         >
-          Learn React
-        </a>
+          <span id="c">c</span><span id="o">o</span><span id="l">l</span><span id="o2">o</span><span id="r">r</span><span>.</span>
+        </button>
+        <p className="cooltext reveal">I'm</p>
+        <div id="name-wrap">
+          <h1 id="first-name">FACUNDO</h1>
+          <h1 id="last-name">TABÁREZ</h1>
+        </div>
+        <p className="cooltext reveal">and I make stuff.</p>
       </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
