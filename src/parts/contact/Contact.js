c5a723e58f8a1c40def834299d633c95667ae73c
falsofacu <facundotabarez0@gmail.com>
2023-07-04 03:09:13 -0300

    Change contact section

--- src/parts/contact/Contact.js
+++ src/parts/contact/Contact.js
@@ -1,83 +1,73 @@
 import { useRef } from "react";
 import "./Contact.css";
-import mailIcon from "../../media/images/contact/envelope-solid.svg";
-import githubIcon from "../../media/images/contact/github.svg";
-import fccIcon from "../../media/images/contact/free-code-camp.svg";
-import codepenIcon from "../../media/images/contact/codepen.svg";
-import likedinIcon from "../../media/images/contact/linkedin.svg";
+import contactJSON from "./Contact.json";
 
 const Contact = () => {
   const mail = useRef("facundotabarez0@gmail.com");
-    
+
   const handleMailClick = () => {
     // Not supported on all browsers
     if (navigator.clipboard && navigator.clipboard.writeText) {
-      navigator.clipboard.writeText(mail.current)
+      navigator.clipboard
+        .writeText(mail.current)
         .then(() => {
-          console.log('Mail copied to clipboard');
+          console.log("Mail copied to clipboard");
         })
         .catch((error) => {
-          console.error('Unable to copy to clipboard:', error);
+          console.error("Unable to copy to clipboard:", error);
         });
-    } 
+    }
     // Deprecated but still supported
     else {
-        const textarea = document.createElement('textarea');
-        textarea.value = mail.current;
-        document.body.appendChild(textarea);
-        textarea.select();
-        textarea.setSelectionRange(0, 100); //Mobile support
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
-        console.log('Mail copied to clipboard');
+      const textarea = document.createElement("textarea");
+      textarea.value = mail.current;
+      document.body.appendChild(textarea);
+      textarea.select();
+      textarea.setSelectionRange(0, 100); //Mobile support
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+      console.log("Mail copied to clipboard");
     }
     alert("Copied mail to clipboard!\n(facundo.tabarez.0@gmail.com)");
   };
 
+  const generateContactList = () => {
+    let contact = [];
+    for (let i = 0; i < contactJSON.length; i++) {
+      if (contactJSON[i].visible) {
+        contact.push(
+          <li 
+            className="contact-element"
+            id={`contact-${contactJSON[i].id}`}
+            key={`contact-${contactJSON[i].id}`}
+            > 
+            <a 
+              href={contactJSON[i].href} 
+              target={contactJSON[i].name === "Mail" ? "_self" : "_blank"}
+              title={contactJSON[i].title}
+              onClick={contactJSON[i].name === "Mail" ? handleMailClick : null}
+            >
+              <img
+                id={contactJSON[i].name}
+                className="contact-icon"
+                src={require(`../../media/images/contact/${contactJSON[i].img}`)}
+                alt={contactJSON[i].title}
+              ></img>
+              {contactJSON[i].name}
+            </a>
+          </li>
+        );
+      }
+    }
+    return contact;
+  };
+
   return (
-    <section id="contact">
+    <div id="contact">
       {/* <h1 className="under-construction">Under Construction</h1> */}
-      <h1 id="contact-title">Find me on:</h1>
-      <div id="contact-sites-container">
-        <a
-          href="mailto:facundotabarez0@gmail.com"
-          onClick={handleMailClick}
-          title="facundotabarez0@gmail.com"
-        >
-          <img id="mail" className="contact-icon" src={mailIcon}></img>
-        </a>
-        {/* 
-            NO LINKEDIN YET, if you got here... well whatever
-        <a href="https://www.linkedin.com/in/facundotabarez/" target="_blank" onClick={handleLinkedInClick}  title="in/facundotabarez/">
-          <img
-            id="likedin"
-            className="contact-icon"
-            src={likedinIcon}
-          ></img>
-        </a> */}
-        <a
-          href="https://github.com/falsofacu"
-          target="_blank"
-          title="GitHub falsofacu"
-        >
-          <img id="github" className="contact-icon" src={githubIcon}></img>
-        </a>
-        <a
-          href="https://codepen.io/falsofacu/"
-          target="_blank"
-          title="Codepen falsofacu"
-        >
-          <img id="codepen" className="contact-icon" src={codepenIcon}></img>
-        </a>
-        <a
-          href="https://www.freecodecamp.org/falsofacu"
-          target="_blank"
-          title="freeCodeCap Certifications"
-        >
-          <img id="fcc" className="contact-icon" src={fccIcon}></img>
-        </a>
-      </div>
-    </section>
+      <h2 id="contact-title">FIND ME</h2>
+      <ul id="contact-sites-container">{generateContactList()}</ul>
+    </div>
   );
 };
 
